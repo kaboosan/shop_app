1d3323b596966a135bad1e0707a69416fd523ad0
Kamel Bourahla <bourahla.kamel@gmail.com>
2021-02-16 06:20:20 +0100

    Add product screen

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -5,7 +5,7 @@ class Product {
   final String title;
   final String description;
   final double price;
-  final String imgUrl;
+  final String imageUrl;
   bool isFavorite;
 
   Product(
@@ -13,6 +13,6 @@ class Product {
       @required this.title,
       @required this.description,
       @required this.price,
-      @required this.imgUrl,
+      @required this.imageUrl,
       this.isFavorite});
 }
