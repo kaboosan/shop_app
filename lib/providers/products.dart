96960530363c1e01089265e67381d4767baff058
Kamel Bourahla <bourahla.kamel@gmail.com>
2021-03-15 05:13:40 +0100

    add firebase conf + migration flutter v2

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import './product.dart';
+import 'dart:convert';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
@@ -65,6 +67,16 @@ class Products with ChangeNotifier {
   }*/
 
   void addProduct(Product product) {
+    const url =
+        'https://shop-app-9d9ff-default-rtdb.europe-west1.firebasedatabase.app/products.json';
+    http.post(url,
+        body: json.encode({
+          'title': product.title,
+          'description': product.description,
+          'imageUrl': product.imageUrl,
+          'price': product.price,
+          'isFavorite': product.isFavorite
+        }));
     final newProduct = Product(
         title: product.title,
         description: product.description,
